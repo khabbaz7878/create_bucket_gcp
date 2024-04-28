e6377a4aa21891e14608d38304de683e8d8e4177
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-04-28 16:29:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,12 @@
-provider "google" {
-  credentials = file("terraform-auth-prod@mcit-capstone-prod.iam.gserviceaccount.com")
-  project     = "mcit-capstone-prod"
-  region      = "us-central1"
-}
+resource "google_storage_bucket" "TF" {
+  name          = "capstone"
+  location      = "US"
+  force_destroy = true
 
-resource "google_storage_bucket" "static" {
-  name         = "gcp_capstone"
-  location      = "us-central1"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-  encryption {
-    default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
-  }
-  depends_on = [
-    google_project_iam_member.default
-  ]
+  
 }
-
-resource "google_storage_bucket_object" "static" {
-  name         = "OBJECT_NAME"
-  source       = "OBJECT_PATH"
-  content_type = "text/plain"
-  bucket       = google_storage_bucket.static.id
+resource "google_storage_bucket_object" "picture" {
+  name   = "gcplogo"
+  source = "/images/nature/garden-tiger-moth.jpg"
+  bucket = create_bucket_gcp.tf.name
 }
